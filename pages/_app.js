b8a1c75927851b6a13ad2ba9e69c83c780c99a41
JazminVazquez <93496074+JazminVazquez@users.noreply.github.com>
2026-03-06 11:37:14 -0300

    Add Google Fonts link to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,22 @@
+import '../styles/globals.css'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
+
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
